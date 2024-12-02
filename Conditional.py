fa81db99c3ac7b2e6ba2adbf26145490183817fa
Shahadat09 <107842646+Shahadat09@users.noreply.github.com>
2024-12-03 00:10:53 +0530

    Add files via upload

---
+++ Conditional.py
@@ -0,0 +1,14 @@
+n=int(input("Enter a no"))
+'''print(n)
+if(n>0):
+    print("Positive")
+elif(n==0):
+    print("Zero")
+else:
+    print("Negative")'''
+
+if(n%5==0):
+    if(n%3==0):
+        print("n is divisible by 3 and 5")
+else:
+    print("n is not divisible by 3 and 5")
