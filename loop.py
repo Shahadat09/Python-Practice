19568302afd57a7c82a3b10b26b5262806af4d6d
Shahadat09 <107842646+Shahadat09@users.noreply.github.com>
2024-12-03 00:11:32 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,11 @@
+'''n=int(input("Enter a no"));
+print(n)
+   for i in range(n):
+    print(i*n)'''
+
+
+'''for i in range(n):
+    print(n,"*",i,"=",n*i)'''
+
+    for i in range(2,10,2):
+        print(i)
